7d8c4defb6185dd07d1fbb4563427264fa811d33
nagendra247 <nagendrasingh247@gmail.com>
2026-06-11 18:30:28 +0530

    new change day 16

--- fs_module/fsasyncawait.jsx
+++ fs_module/fsasyncawait.jsx
@@ -14,15 +14,63 @@ const readFolder= async()=>{
 }
 // readFolder();
 
+
 const filename ="fsasyncawait.txt";
 const filepath = path.join(__dirname,filename);
 // crate file by using async await
 const createFile= async()=>{
     try{
-        const file = await fs.writeFile(filepath,"hello , this file is created by using async await");
+        const file = await fs.writeFile(filepath,"hello , this file is created by using async await","utf-8");
         console.log("file created successfully",file);
     }catch(err){
         console.error(err);
     }
 }
-createFile();
+// createFile();
+
+// read file by using async await
+const readFile= async()=>{
+    try{
+        const file = await fs.readFile(filepath,"utf-8");
+        console.log("file read successfully",file);
+    }catch(err){
+        console.error(err);
+    }
+}
+readFile();
+
+// update file by using async await
+const updateFile= async()=>{
+    try{
+        const file = await fs.appendFile(filepath  ," this file is updated by using async await","utf-8");
+        console.log("file updated successfully",file);
+    }catch(err){
+        console.error(err);
+    }   
+}
+updateFile();
+
+//rename fileby using async await
+// const oldfilename = "fsasyncawait.txt";
+const newfilename = "fsasyncawaitupdated.txt";
+const renamefile= async()=>{
+    try{
+        const file = await fs.rename(filepath,path.join(__dirname,newfilename));
+        console.log("file renamed successfully",file);
+    }catch(err){
+        console.error(err);
+    }
+
+}
+renamefile();
+
+//delete file by using async await
+ const deletefile =  async ()=>{
+    try{
+        const file = await fs.unlink(newfilename);
+        console.log("file deleted successfully",file);
+    }catch(err){
+        console.log(err)
+    }
+ }
+// deletefile();
